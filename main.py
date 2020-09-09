a58c2e8c95cdcd7c4a2695dcc4df1a55254926d7
Alan-Kuan <kuanhaoan@gmail.com>
2020-09-09 21:42:39 +0800

    Add a license notice on the top of main.py.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+#      YT Downloader - A simple GUI for Python package, youtube-dl.
+#      Copyright (C) 2019, 2020 Alan Kuan
+#
+#	   This file is a part of YT Downloader.
+#  
+#      YT Downloader is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#  
+#      YT Downloader is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#  
+#      You should have received a copy of the GNU General Public License
+#      along with YT Downloader.  If not, see <https://www.gnu.org/licenses/>.
+
 import sys
 
 from PyQt5.QtWidgets import QDialog, QFileDialog, QApplication, QMessageBox, QLabel
